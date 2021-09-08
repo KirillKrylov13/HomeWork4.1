e1d5ca7cd912903375979864f0ff9c2d61f5e0f8
KirillKrylov13 <85196548+KirillKrylov13@users.noreply.github.com>
2021-09-08 11:16:31 +0300

    Add files via upload

---
+++ HomeWork4.2/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> MyIntVector{ 2,8,4,1,4,9,5,8,5,5 };
+    sort(MyIntVector.begin(), MyIntVector.end());       //Сортировка от наименьшего к большему . ИТОГ : 1 2 4 4 5 5 5 8 8 9
+    vector<int> :: iterator iter = MyIntVector.begin(); //Объявление итератора и установка его на начальный элемент вектора
+                            
+    size_t m=1;                                         //Переменная счетчик,для подсчета различных чисел
+        do
+        {  
+            if (*iter != *(iter + 1))
+            {
+                m++;            
+            }
+            iter++; 
+        } while (iter != MyIntVector.end()-1);
+    cout << "\nNumbers: "<<m;                           //Вывод кол-ва различных чисел,находящихся в векторе
+    return 0;
+}
