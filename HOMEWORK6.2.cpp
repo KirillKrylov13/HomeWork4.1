0f3a028f01d4e24af20b94039232fab4d1b5fc1a
KirillKrylov13 <85196548+KirillKrylov13@users.noreply.github.com>
2021-09-17 15:11:49 +0300

    Add files via upload

---
+++ HOMEWORK6.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename CharT, typename Traits >
+basic_ostream<CharT, Traits>& endll(basic_ostream<CharT, Traits>& os)
+{
+    os.put(os.widen('\n'));
+    os.put(os.widen('\n'));
+    os.flush();
+
+    return os;
+}
+
+int main()
+{
+    cout << "Red" << endll;
+    cout << "Blue";
+    return 0;
+}
---
+++ inputOutput.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int GetIntNumb()
+{
+	while (true)
+	{
+		cout << "Enter an integer value: ";
+		int num;
+		//cin >> num;
+
+		if (!(cin>>num))
+		{
+
+			cout << "Error. Please enter an integer number!" << endl;
+			cin.clear();
+			cin.ignore(32767, '\n');
+		}
+
+		else
+		{
+			return num;
+		}
+	}
+}
+
+
+int main()
+{
+	cout << "Your int number: " << GetIntNumb() << endl;
+	return 0;
+}
+
